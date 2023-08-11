78160611db3aa51d9485e79228994c545ba12710
Italo Amaya <italo@Italos-MacBook-Pro.local>
2023-08-11 11:21:52 -0500

    Ready for publish

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function goHome() {
 
   // Hide the content
   $('#content').hide();
+  $('#about').hide();
+  $('#blog').hide();
   $('#content').removeClass("animate__fadeOutRight");
 }
 
@@ -19,9 +21,9 @@ function about() {
   $('#0').show();
   $('#0').addClass("animate__fadeInLeft");
 
-  // Replace the content for About me
-  $('#content').text("I am a computer Science Student in the University of Leeds. My goal is to work in the field of Cybersecurity. On the side I like to cook and program little projects.");
+  // show the aobut content
   $('#content').show();
+  $('#about').show();
   $('#content').addClass("animate__fadeInRight");
 }
 
@@ -34,14 +36,6 @@ function goto(id) {
     case "1": // About Me
       about();
       break;
-    case "2": // Blog
-      break;
-    case "3": // Recipes
-      break;
-    case "4": // Projects
-      break;
-    case "5": // Curriculum Vitae
-      break;
   }
 }
 
@@ -50,6 +44,8 @@ function goto(id) {
 $('nav button, nav a').on("click", function (event) {
   // Remove delays for animations
   $('nav button, nav a').removeClass("animate__delay-2s");
+  $('#name').removeClass("animate__delay-2s");
+  $('#quote').removeClass("animate__delay-3s");
 
   // Animate exits
   $('nav button, nav a').addClass("animate__fadeOutRight");
